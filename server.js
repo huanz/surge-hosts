0d14eeba8e3c7a7155ae346b69c0956960ba3685
huanz <260540233@qq.com>
2016-05-01 20:54:58 +0800

    add console env

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ AV.Cloud.useMasterKey();
 var app = require('./app');
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
+console.log(process.env);
 var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 app.listen(PORT, function () {
     console.log('surge hosts update app is running, port:', PORT);
