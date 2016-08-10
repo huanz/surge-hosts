d66501423835578b64d8d678c5c8b022a229efc0
bukas <yhz1219@gmail.com>
2016-08-10 17:01:03 +0800

    set timezone to Asia/Chongqing

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = require('./app');
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LC_APP_PORT || 3000);
+process.env.TZ = 'Asia/Chongqing';
 app.listen(PORT, function () {
     console.log('surge hosts update app is running, port:', PORT);
 });
