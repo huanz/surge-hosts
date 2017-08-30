f3036f0a72ba21758ae6a95c4c9a771470ea398f
bukas <yhz1219@gmail.com>
2017-08-30 09:56:09 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 var fs = require('fs');
-var domain = require('domain');
 var express = require('express');
+var timeout = require('connect-timeout');
 var path = require('path');
 var bodyParser = require('body-parser');
 var AV = require('leanengine');
@@ -16,36 +16,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// 加载云代码方法
-// app.use(cloud);
+// 设置默认超时时间
+app.use(timeout('15s'));
 
-// 使用 LeanEngine 中间件
-// （如果没有加载云代码方法请使用此方法，否则会导致部署失败，详细请阅读 LeanEngine 文档。）
-app.use(AV.Cloud);
+// 加载云引擎中间件
+app.use(AV.express());
+
+app.enable('trust proxy');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// 未处理异常捕获 middleware
-app.use(function (req, res, next) {
-    var d = null;
-    if (process.domain) {
-        d = process.domain;
-    } else {
-        d = domain.create();
-    }
-    d.add(req);
-    d.add(res);
-    d.on('error', function (err) {
-        console.error('uncaughtException url=%s, msg=%s', req.url, err.stack || err.message || err);
-        if (!res.finished) {
-            res.statusCode = 500;
-            res.setHeader('content-type', 'application/json; charset=UTF-8');
-            res.end('uncaughtException');
-        }
-    });
-    d.run(next);
-});
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.get('/', function (req, res) {
     res.render('index', status);
@@ -68,37 +50,37 @@ app.get('/', function (req, res) {
     res.render('index', status);
 });
 
-// 如果任何路由都没匹配到，则认为 404
-// 生成一个异常让后面的 err handler 捕获
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
+    if (!res.headersSent) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
 });
-
 // error handlers
-
-// 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) { // jshint ignore:line
-        var statusCode = err.status || 500;
-        if (statusCode === 500) {
-            console.error(err.stack || err);
-        }
-        res.status(statusCode);
-        res.render('error', {
-            message: err.message || err,
-            error: err
-        });
-    });
-}
-
-// 如果是非开发环境，则页面只输出简单的错误信息
-app.use(function (err, req, res, next) { // jshint ignore:line
-    res.status(err.status || 500);
+app.use(function (err, req, res, next) {
+    if (req.timedout && req.headers.upgrade === 'websocket') {
+        // 忽略 websocket 的超时
+        return;
+    }
+    var statusCode = err.status || 500;
+    if (statusCode === 500) {
+        console.error(err.stack || err);
+    }
+    if (req.timedout) {
+        console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
+    }
+    res.status(statusCode);
+    // 默认不输出异常详情
+    var error = {};
+    if (app.get('env') === 'development') {
+        // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
+        error = err;
+    }
     res.render('error', {
-        message: err.message || err,
-        error: {}
+        message: err.message,
+        error: error
     });
 });
 
